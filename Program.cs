b9927883dc2b7799856ec63383d4d337423cdaee
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-26 19:17:59 +0500

    ешил задачу с полиндромом

--- Program.cs
+++ Program.cs
@@ -5,17 +5,35 @@
 // 12821 -> да
 // 23432 -> да
 
-// Конд на питоне. на вход даём число,
-// def is_pal(n):
-//     nn=n
-//     rn=0
-//     while(n>0):
-//         rn=rn*10+n%10
-//         n=n//10
-//     return nn==rn
+void FindPolindrom (int number)
+{
+    // Проверка чиcла пользователя
+    if (number != 0 && number % 10 != 0 && number > 0)
+    {
+        int newNumber = 0; // Новое число в обратном порядке
+        int preNumber = number; // Новая переменная для хранения остатков от действий
+        while ( number > newNumber ) // пока не дошли до середине делаем
+        {
+            int rightNum = number % 10; // берём самое правое число от начального значения ( 12345 % 10 -> 5)
+            preNumber = number; // записываем оставшееся число после одного цикла  (1234)
+            number = number / 10; // отбираем последнюю цифру от нашего числа ( 12345/10 -> 1234 ) 
+            newNumber = newNumber * 10 + rightNum; // умножая на 10 увеличиваем разряд и прибавляем новое цисло
+        }
+        // 1. Если обратное число совпало со старым (нечетные разряды)
+        // 2. Если остаток от деления (preNumber) равен новому числу (четные разпяды)
+        if(number == newNumber || preNumber == newNumber) 
+        {
+            Console.WriteLine("Ваше число полиндром");
+        }
+        else Console.WriteLine("Ваше число НЕ полиндром");
 
+    }
+    else Console.WriteLine("Ваше число нельзя проверить");
+}
 
-
+Console.Write("Введите ваше число: "); 
+int number = Convert.ToInt32(Console.ReadLine());
+FindPolindrom(number);
 
 
 
@@ -24,6 +42,8 @@
 
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+void Find
 
 
 
